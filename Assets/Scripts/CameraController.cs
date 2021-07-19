e12e4593a9d2f9e4b5938189f3ca229c58dae39d
BrynDev <bryngamedev@gmail.com>
2021-07-19 02:05:50 +0200

    Finished basic movement and camera controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+    [SerializeField]
+    private Transform m_Player;
+    [SerializeField]
+    private float m_CameraSpeed = 1;
+
+    private float m_TotalMouseX;
+    private float m_TotalMouseY;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        m_TotalMouseX += Input.GetAxis("MouseX") * m_CameraSpeed;
+        m_TotalMouseY -= Input.GetAxis("MouseY") * m_CameraSpeed;
+        m_TotalMouseY = Mathf.Clamp(m_TotalMouseY, -75, 60);
+        transform.LookAt(m_Target);
+       
+        m_Target.rotation = Quaternion.Euler(m_TotalMouseY, m_TotalMouseX, 0);
+
+        m_Player.rotation = Quaternion.Euler(0, m_TotalMouseX, 0);
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MoveSpeed = 1;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       float movementForward = Input.GetAxis("Forward");
+       float movementRight = Input.GetAxis("Right");
+       Vector3 totalMovement = new Vector3(movementRight, 0.0f, movementForward) * m_MoveSpeed * Time.deltaTime;
+       transform.Translate(totalMovement);
+    }
+
+    
+}

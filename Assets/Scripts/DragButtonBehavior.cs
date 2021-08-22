281efe1fe2c6d096c90acf4564c1da687b29fd8c
BrynDev <bryngamedev@gmail.com>
2021-08-22 20:30:46 +0200

    Attempted fix to HotbarSlot issue
    This reverts commit 09dba1baeb691df9d2f785c4645a8a049e5cf6e9.

--- Assets/Scripts/DragButtonBehavior.cs
+++ Assets/Scripts/DragButtonBehavior.cs
@@ -5,25 +5,7 @@ using UnityEngine;
 public class DragButtonBehavior : Clickable
 {
     private bool m_IsDragged = false;
-    private List<Transform> m_HotbarElements = new List<Transform>(); 
-
-    private void Start()
-    {
-        Transform parentTransform = transform.parent;
-        int nrChildren = parentTransform.childCount;
-       
-        //store all hotbar slots that are a child to this drag button's parent (aka get every slot in this hotbar)
-        for (int i = 0; i < nrChildren; ++i)
-        {
-            Transform childObject = parentTransform.GetChild(i);
-            //don't store this drag button
-            if(childObject != transform)
-            {
-                m_HotbarElements.Add(childObject);
-            }
-        }
-    }
-
+    
     public override void OnLeftMouseButtonDown()
     {
         Debug.Log("Drag button hit");
@@ -43,14 +25,7 @@ public class DragButtonBehavior : Clickable
             return;
         }
 
-        Vector3 previousPos = transform.position;
         transform.position = Input.mousePosition;
-
-        Vector3 deltaPos = transform.position - previousPos;
-        foreach(Transform hotbarSlot in m_HotbarElements)
-        {
-            hotbarSlot.transform.position += deltaPos;
-        }
     }
 
 }

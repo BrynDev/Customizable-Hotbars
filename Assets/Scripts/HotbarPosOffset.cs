c1a9821b1ae2936517ac4fa86b6613d2dd82c9a4
BrynDev <bryngamedev@gmail.com>
2021-09-30 00:36:49 +0200

    Added HotbarPosOffset script
    This script is used to easily setup a screenspace pos offset to hotbars, to make sure that multiple hotbars don't properly overlap when the game starts.

---
+++ Assets/Scripts/HotbarPosOffset.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotbarPosOffset : MonoBehaviour
+{
+    // Since the position of the hotbar is defined in the prefab, this script is used to easily assign a screen space position offset from the editor,
+    // so that multiple hotbars dont overlap perfectly when the game starts.
+
+
+    [SerializeField]
+    private Vector3 m_Offset;
+    void Start()
+    {
+        Canvas canvas = GetComponentInChildren<Canvas>();
+        int nrChildren = canvas.transform.childCount;
+        for(int i = 0; i < nrChildren; ++i)
+        {
+            canvas.transform.GetChild(i).transform.position += m_Offset;
+        }
+    }
+}
